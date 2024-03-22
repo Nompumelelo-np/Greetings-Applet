67a88f2df3d40faa898f893b36150392fbbaf6c7
Nompumelelo Hlongwane <61636340+Nompumelelo-np@users.noreply.github.com>
2024-03-22 23:41:35 +0200

    Add files via upload

---
+++ GreetingsApplet/src/main/java/com/mycompany/greetingsapplet/GreetingsApplet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.greetingsapplet;
+
+/**
+ *
+ * @author Acer-User
+ */
+public class GreetingsApplet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
